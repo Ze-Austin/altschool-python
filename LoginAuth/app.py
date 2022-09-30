f2310594430bf4536edd82841451ef42be1fc635
Austin Wopara <austinwopara@gmail.com>
2022-09-30 18:10:31 +0100

    minor tweaks

--- LoginAuth/app.py
+++ LoginAuth/app.py
@@ -8,8 +8,7 @@ base_dir = os.path.dirname(os.path.realpath(__file__))
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + \
-    os.path.join(base_dir, 'my_login.db')
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(base_dir, 'my_login.db')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
