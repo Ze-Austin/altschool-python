ef0440e29a241efd5bdb1d296404b6901023629a
Austin Wopara <austinwopara@gmail.com>
2022-12-14 19:33:18 +0100

    Update Shop project after Wednesday class

--- Shop/resources/user.py
+++ Shop/resources/user.py
@@ -5,6 +5,7 @@ from passlib.hash import pbkdf2_sha256
 from models import UserModel
 from db import db
 from schemas import UserSchema
+from flask_jwt_extended import create_access_token
 
 blp = Blueprint("Users", "users", description="Operations on Users")
 
@@ -24,3 +25,41 @@ class UserRegister(MethodView):
         db.session.commit()
 
         return {"message": "User Created Successfully"}, 201
+
+@blp.route("/login")
+class Login(MethodView):
+    @blp.arguments(UserSchema)
+    def post(self, user_data):
+        user = UserModel.query.filter(
+            UserModel.username == user_data["username"]
+        ).first()
+
+        if user and pbkdf2_sha256.verify(user_data["password"], user.password):
+            access_token = create_access_token(identity=user.id)
+
+            return {"access_token": access_token}
+        
+        abort(401, message="Invalid Credentials")
+
+@blp.route("/user/<int:user_id>")
+class User(MethodView):
+    # Get a user by id
+    @blp.response(200, UserSchema)
+    def get(self, user_id):
+        user = UserModel.query.get_or_404(user_id)
+        return user
+    
+    # Delete a user by id
+    def delete(self, user_id):
+        user = UserModel.query.get_or_404(user_id)
+        db.session.delete(user)
+        db.session.commit()
+
+        return {"message": "User Deleted"}, 200
+
+@blp.route("/users")
+class UserList(MethodView):
+    @blp.response(200, UserSchema(many=True))
+    def get(self):
+        all_users = UserModel.query.all()
+        return all_users
