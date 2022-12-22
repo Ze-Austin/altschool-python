f9c6a6ea4325eeb0dec150a792e88c12f27b25df
Austin Wopara <austinwopara@gmail.com>
2022-12-22 09:19:58 +0100

    Update after Last Class of 2022

--- Caleb/shoprite/db.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-
-
-# stores = {}
-# items = {}
--- Caleb/shoprite/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from models.item import ItemModel
-from models.store import StoreModel
