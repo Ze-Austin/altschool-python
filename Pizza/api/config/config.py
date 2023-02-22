0e8f948cac6c6a6d0dba46c674bc55319f57fd8a
Austin Wopara <austinwopara@gmail.com>
2023-02-22 18:55:20 +0100

    Update Pizza API after Wednesday class

--- Pizza/api/config/config.py
+++ Pizza/api/config/config.py
@@ -4,6 +4,10 @@ from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
+uri = os.getenv('DATABASE_URL') # or other relevant config var
+if uri.startswith('postgres://'):
+    uri = uri.replace('postgres://', 'postgresql://', 1)
+
 class Config:
     SECRET_KEY = config('SECRET_KEY', 'secret')
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)
@@ -24,7 +28,9 @@ class TestConfig(Config):
 
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = uri
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = config('DEBUG', False, cast=bool)
 
 
 config_dict = {
--- Pizza/runserver.py
+++ Pizza/runserver.py
@@ -1,6 +1,7 @@
 from api import create_app
+from api.config.config import config_dict
 
-app = create_app()
+app = create_app(config=config_dict['prod'])
 
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
